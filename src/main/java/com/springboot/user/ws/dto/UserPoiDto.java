fdbd7a2131a3b1f01851bcd2c681b87782a1c571
zhanglong <1679937491@qq.com>
2017-03-11 16:35:07 +0800

    poi  测试类

---
+++ src/main/java/com/springboot/user/ws/dto/UserPoiDto.java
@@ -0,0 +1,8 @@
+package com.springboot.user.ws.dto;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+public class UserPoiDto {
+
+}
---
+++ src/test/java/com/springboot/common/PoiTest.java
@@ -0,0 +1,20 @@
+package com.springboot.common;
+
+import com.springboot.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class PoiTest {
+
+    @Test
+    public  void uploadUserMessageTest(){
+
+    }
+}
