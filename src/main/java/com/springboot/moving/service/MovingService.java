bfea8f05c28a2b6a42db88b3646512d02ca1cf26
zhanglong <zhanglong@eshippinggateway.com>
2017-03-15 17:51:59 +0800

    分页

--- src/main/java/com/springboot/moving/service/MovingService.java
+++ src/main/java/com/springboot/moving/service/MovingService.java
@@ -74,7 +74,7 @@ public class MovingService {
     public ListResponseDto<MovingDto> listMoving(int offset,int size) {
         ListResponseDto<MovingDto> listResponseDto = new ListResponseDto<>();
         List<MovingDto> listMovingDto = Lists.newArrayList();
-        List<Moving> listMoving = movingDao.listMoving(offset*size-1,size);
+        List<Moving> listMoving = movingDao.listMoving((offset-1)*size,size);
         listMoving.stream().forEach(p -> listMovingDto.add(formatMoving(p)));
         listResponseDto.setObjs(listMovingDto);
         return listResponseDto;
